721df6230292bc878cd8d489faeea660d69669a3
yairJCT <irigo1@gmail.com>
2019-11-26 13:14:11 +0200

    Exception

---
+++ week5/TimeTravelException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week5
+{
+    class TimeTravelException : Exception
+    {
+        public TimeTravelException()
+        {
+        }
+
+        public TimeTravelException(string message) : base(message)
+        {
+        }
+
+        public TimeTravelException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TimeTravelException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
